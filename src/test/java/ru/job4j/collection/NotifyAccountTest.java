acea074ff790c71e12a648600c8b416722f933ae
KonstantinDolgov <dolgovkos777@gmail.com>
2022-10-11 22:30:09 +0300

    3. Модели данных и HashSet. [#218530]

---
+++ src/test/java/ru/job4j/collection/NotifyAccountTest.java
@@ -0,0 +1,48 @@
+package ru.job4j.collection;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class NotifyAccountTest {
+    @Test
+    public void whenTwoAccountsAddSent() {
+        NotifyAccount notifyAccount = new NotifyAccount();
+        List<Account> accounts = Arrays.asList(
+                new Account("123", "Petr Arsentev", "eDer3432f"),
+                new Account("142", "Petr Arsentev", "000001")
+        );
+        HashSet<Account> expect = new HashSet<>(
+                Arrays.asList(
+                        new Account("123", "Petr Arsentev", "eDer3432f"),
+                        new Account("142", "Petr Arsentev", "000001")
+                )
+        );
+        assertThat(notifyAccount.sent(accounts)).containsAll(expect);
+    }
+
+    @Test
+    public void whenDeleteDuplicate() {
+        NotifyAccount notifyAccount = new NotifyAccount();
+        List<Account> accounts = Arrays.asList(
+                new Account("123", "Petr Arsentev", "eDer3432f"),
+                new Account("123", "Petr Arsentev", "eDer3432f"),
+                new Account("142", "Petr Arsentev", "000001"),
+                new Account("142", "Petr Arsentev", "000001"),
+                new Account("777", "K D", "100500"),
+                new Account("777", "K D", "100500")
+        );
+        HashSet<Account> expect = new HashSet<>(
+                Arrays.asList(
+                        new Account("123", "Petr Arsentev", "eDer3432f"),
+                        new Account("142", "Petr Arsentev", "000001"),
+                        new Account("777", "K D", "100500")
+                )
+        );
+        assertThat(notifyAccount.sent(accounts)).isEqualTo(expect);
+    }
+}
