a1c41dfe9d7c841d640e0cd670bf8b7b76fb82ea
KonstantinDolgov <dolgovkos777@gmail.com>
2022-11-01 20:31:25 +0300

    1. Организовать сортировку User [#10034 #356390]

--- src/main/java/ru/job4j/collection/JobSorter.java
+++ src/main/java/ru/job4j/collection/JobSorter.java
@@ -2,7 +2,6 @@ package ru.job4j.collection;
 
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 public class JobSorter {
@@ -15,14 +14,7 @@ public class JobSorter {
         System.out.println(jobs);
         Collections.sort(jobs);
         System.out.println(jobs);
-        Collections.sort(jobs, new JobDescByName().thenComparing(new JobDescByPriority()));
-        System.out.println(jobs);
         jobs.sort(new SortDescByNameJob());
-        System.out.println(jobs);
-        Comparator<Job> comb = new JobDescByNameLn()
-                .thenComparing(new JobDescByName())
-                .thenComparing(new JobDescByPriority());
-        Collections.sort(jobs, comb);
         System.out.println(jobs);
     }
 }
