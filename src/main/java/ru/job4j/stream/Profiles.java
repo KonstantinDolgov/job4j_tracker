63ba6c6387fe2eddae16a7c0ae272bbaab81c0ea
KonstantinDolgov <dolgovkos777@gmail.com>
2022-12-14 19:44:42 +0300

    6. Список адресов [#110062 #370913]

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -1,25 +1,13 @@
 package ru.job4j.stream;
 
-import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Profiles {
 
     public static List<Address> collect(List<Profile> profiles) {
         return profiles.stream()
-                .flatMap(Stream::ofNullable)
                 .map(Profile::getAddress)
-                .collect(Collectors.toList());
-    }
-
-    public static List<Address> collectSortWithoutDuplicate(List<Profile> profiles) {
-        return profiles.stream()
-                .flatMap(Stream::ofNullable)
-                .sorted(Comparator.comparing(profile -> profile.getAddress().getCity()))
-                .map(Profile::getAddress)
-                .distinct()
                 .collect(Collectors.toList());
     }
 }
